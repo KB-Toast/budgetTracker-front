f9d9f2ca92c8d32693448d1edbb8e291f96ed174
KB-Toast <kevin.brusson@gmail.com>
2024-02-20 16:27:09 +0100

    Getting started

---
+++ components/Dashboard.js
@@ -0,0 +1,12 @@
+
+
+
+function Dashboard() {
+    return (
+        <div>
+            Dashboard component
+        </div>
+    )
+}
+
+export default Dashboard;
---
+++ components/Home.js
@@ -0,0 +1,12 @@
+import styles from '../styles/Home.module.css';
+import Link from 'next/link';
+
+function Home() {
+  return (
+    <div>
+      <Link href="/dashboard"> Dashboard </Link>
+    </div>
+  );
+}
+
+export default Home;
